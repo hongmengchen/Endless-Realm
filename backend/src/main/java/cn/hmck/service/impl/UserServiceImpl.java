f905864d70771127ee4d0c5d9c971bd1b2b5757a
hongmengchen <2560174991@qq.com>
2024-12-03 21:59:51 +0800

    refactor(service): 为 UserServiceImpl 类指定服务 ID
    - 在 @Service 注解中添加 "userService" 作为服务 ID
    - 这样做可以提高项目的可维护性，明确指定服务层组件

--- backend/src/main/java/cn/hmck/service/impl/UserServiceImpl.java
+++ backend/src/main/java/cn/hmck/service/impl/UserServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
+@Service("userService")
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
     // 注入 UserMapper
     private final UserMapper userMapper;
