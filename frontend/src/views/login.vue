98039321c46c84dd9590b2824e7120c719ae85c4
hongmengchen <2560174991@qq.com>
2024-12-07 01:11:17 +0800

    feat(login): 重构用户登录页面
    - 设计了新的登录页面布局和样式，提升了用户体验
    - 优化了登录表单，增加了图标和键盘事件处理
    - 添加了注册和管理员登录的链接- 重构了登录逻辑，提高了代码可读性和维护性

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,78 +1,129 @@
 <template>
-  <el-container style="height: 100vh">
-    <el-row type="flex" justify="center" align="middle" style="height: 100%">
-      <el-col :span="8">
-        <el-card>
-          <h2 style="text-align: center">用户登录</h2>
-          <el-form :model="form" label-width="100px" ref="form">
-            <el-form-item label="用户名" prop="username" :rules="usernameRules">
-              <el-input
-                v-model="form.username"
-                placeholder="请输入用户名"
-              ></el-input>
-            </el-form-item>
-
-            <el-form-item label="密码" prop="password" :rules="passwordRules">
-              <el-input
-                v-model="form.password"
-                type="password"
-                placeholder="请输入密码"
-              ></el-input>
-            </el-form-item>
-
-            <el-form-item>
-              <el-button
-                type="primary"
-                @click="login"
-                :loading="loading"
-                :disabled="loading"
-              >
-                登录
-              </el-button>
-            </el-form-item>
-          </el-form>
-        </el-card>
-      </el-col>
-    </el-row>
-  </el-container>
+  <div class="login-container">
+    <el-card class="box-card">
+      <div class="login-body">
+        <div class="login-title" @click="toIndex">Endless Realm · 无垠之界</div>
+        <el-form ref="form" :model="userForm">
+          <el-input
+            placeholder="请输入账号..."
+            v-model="userForm.username"
+            class="login-input"
+          >
+            <template v-slot:prepend>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
+          <el-input
+            placeholder="请输入密码..."
+            v-model="userForm.password"
+            class="login-input"
+            @keyup.enter="login"
+            show-password
+          >
+            <template v-slot:prepend>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+          <div class="login-submit">
+            <el-button type="primary" @click="login">登录</el-button>
+          </div>
+          <div class="other-submit">
+            <router-link to="/register" class="sign-in-text">注册</router-link>
+            <router-link to="/login-admin" class="sign-in-text"
+              >管理员登录</router-link
+            >
+          </div>
+        </el-form>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
-import User from "@/api/user";
+import UserAPI from "@/api/user";
+import { Lock, User } from "@element-plus/icons-vue";
 
 export default {
-  name: "LoginPage",
+  name: "userLoginPage",
+  components: { Lock, User },
   data() {
     return {
-      form: {
+      userForm: {
         username: "张三",
         password: "123456",
       },
-      loading: false,
-      usernameRules: [
-        { required: true, message: "请输入用户名", trigger: "blur" },
-      ],
-      passwordRules: [
-        { required: true, message: "请输入密码", trigger: "blur" },
-      ],
     };
   },
   methods: {
+    // 用户登录
     async login() {
-      const res = await User.login(this.form.username, this.form.password);
+      const res = await UserAPI.login(
+        this.userForm.username,
+        this.userForm.password
+      );
       console.log(res);
       if (res.data.status_code == 1) {
         console.log("登录成功");
         this.$router.push("/");
       } else {
-        console.log("登录失败");
-        this.$router.push("/login");
+        this.$message.error(res.msg);
       }
+    },
+    // 跳转首页
+    toIndex() {
+      this.$router.replace({ path: "/" });
     },
   },
 };
 </script>
 
 <style scoped>
-/* 样式 */
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  background-color: #f1f1f1;
+}
+
+.login-body {
+  padding: 30px;
+  width: 400px;
+  height: 100%;
+}
+
+.login-title {
+  padding-bottom: 30px;
+  text-align: center;
+  font-weight: 600;
+  font-size: 20px;
+  color: #409eff;
+  cursor: pointer;
+}
+
+.login-input {
+  margin-bottom: 20px;
+}
+
+.login-submit {
+  display: flex;
+  justify-content: center;
+}
+
+.sign-in-container {
+  padding: 0 10px;
+}
+
+.sign-in-text {
+  color: #409eff;
+  font-size: 16px;
+  text-decoration: none;
+  line-height: 28px;
+}
+.other-submit {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+}
 </style>
