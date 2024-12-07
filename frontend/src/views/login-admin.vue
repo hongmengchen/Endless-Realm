ba280f20f6988576aa42100ffbcb50f65a9d2e7b
hongmengchen <2560174991@qq.com>
2024-12-08 01:21:44 +0800

    feat: 添加管理员登录页面
    - 新增 login-admin.vue 组件，用于管理员登录
    - 实现了管理员登录的基本功能，包括用户名和密码输入、登录按钮和用户登录链接
    - 添加了简单的表单验证和错误提示
    - 优化了登录页面的样式和布局

---
+++ frontend/src/views/login-admin.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="login-container">
+    <el-card class="box-card">
+      <div class="login-body">
+        <div class="login-title">后台管理</div>
+        <el-form ref="form" :model="adminForm">
+          <el-input
+            placeholder="请输入管理员账号"
+            v-model="adminForm.username"
+            class="login-input"
+          >
+            <template v-slot:prepend>
+              <el-icon><User /></el-icon>
+            </template>
+          </el-input>
+          <el-input
+            placeholder="请输入管理员密码"
+            v-model="adminForm.password"
+            class="login-input"
+            @keyup.enter="login"
+            show-password
+          >
+            <template v-slot:prepend>
+              <el-icon><Lock /></el-icon>
+            </template>
+          </el-input>
+          <div class="login-submit">
+            <el-button type="primary" @click="login">登录</el-button>
+          </div>
+          <div class="other-submit">
+            <router-link to="/login" class="sign-in-text">用户登录</router-link>
+          </div>
+        </el-form>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import adminAPI from "@/api/adminAPI";
+import { Lock, User } from "@element-plus/icons-vue";
+
+export default {
+  name: "adminLoginPage",
+  components: { Lock, User },
+  data() {
+    return {
+      adminForm: {
+        username: "admin",
+        password: "password",
+      },
+    };
+  },
+  methods: {
+    // 管理员登录
+    async login() {
+      const res = await adminAPI.login(
+        this.adminForm.username,
+        this.adminForm.password
+      );
+      if (res.data.status_code === 1) {
+        console.log(res);
+      } else {
+        this.$message.error(res.data.msg);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  background-color: #f1f1f1;
+}
+.login-body {
+  padding: 30px;
+  width: 400px;
+  height: 100%;
+}
+.login-title {
+  padding-bottom: 30px;
+  text-align: center;
+  font-weight: 600;
+  font-size: 20px;
+  color: #409eff;
+  cursor: pointer;
+}
+.login-input {
+  margin-bottom: 20px;
+}
+.login-submit {
+  display: flex;
+  justify-content: center;
+}
+.sign-in-text {
+  color: #409eff;
+  font-size: 16px;
+  text-decoration: none;
+  line-height: 28px;
+}
+.other-submit {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+}
+</style>
