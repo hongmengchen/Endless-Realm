da6e369428aaa637a9157ab5d524b6eaf4d58df8
hongmengchen <2560174991@qq.com>
2024-12-08 01:20:37 +0800

    feat(router): 添加管理员和用户路由
    - 在路由配置中引入 adminRoute 和 userRoute 模块- 将新路由合并到主路由数组中

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import baseRouters from "./modules/base";
+import adminRoute from "./modules/adminRoute";
+import userRoute from "./modules/userRoute";
 
-const routes = [...baseRouters];
+const routes = [...baseRouters, ...adminRoute, ...userRoute];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
