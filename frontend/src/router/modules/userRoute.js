29e56485a20ecca492e57be567914fe1d877f7e8
hongmengchen <2560174991@qq.com>
2024-12-08 17:06:43 +0800

    feat(router): 为用户相关页面添加浏览器标签页标题
    - 在 login、register 和 profile 路由中添加 meta.title 属性- 优化了浏览器标签页的显示效果，提升用户体验

--- frontend/src/router/modules/userRoute.js
+++ frontend/src/router/modules/userRoute.js
@@ -5,17 +5,26 @@ export default [
     path: "/login",
     name: "login",
     component: () => import("@/views/login.vue"),
+    meta: {
+      title: "登录 | Endless Realm · 无垠之界",
+    },
   },
   // 定义注册页面的路由
   {
     path: "/register",
     name: "register",
     component: () => import("@/views/userRegister.vue"),
+    meta: {
+      title: "注册 | Endless Realm · 无垠之界",
+    },
   },
   // 定义个人资料页面的路由
   {
     path: "/profile",
     name: "profile",
     component: () => import("@/views/profile.vue"),
+    meta: {
+      title: "个人资料 | Endless Realm · 无垠之界",
+    },
   },
 ];
