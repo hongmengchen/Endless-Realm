3fdb199134b20717f602882b06eca4d073600f54
hongmengchen <2560174991@qq.com>
2024-12-08 20:59:14 +0800

    feat(router): 添加 post 路由模块
    - 在 router/index.js 中引入 postRoute 模块- 将 postRoute 添加到 routes 数组中

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,8 +3,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import baseRouters from "./modules/base";
 import adminRoute from "./modules/adminRoute";
 import userRoute from "./modules/userRoute";
+import postRoute from "./modules/postRoute";
 
-const routes = [...baseRouters, ...adminRoute, ...userRoute];
+const routes = [...baseRouters, ...adminRoute, ...userRoute, ...postRoute];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
