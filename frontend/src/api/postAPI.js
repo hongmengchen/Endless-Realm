10674d0463daccfc6b2dd408c63ac5a9193272d9
hongmengchen <2560174991@qq.com>
2024-12-09 21:40:09 +0800

    refactor(postAPI): 重构帖子接口并添加发布动态功能
    - 移除了不必要的 get 请求方法- 引入了 post 请求方法
    - 删除了未使用的接口函数
    - 新增了 addPost 方法用于发布动态

--- frontend/src/api/postAPI.js
+++ frontend/src/api/postAPI.js
@@ -1,15 +1,7 @@
 import { get } from "@/utils/request";
+import { post } from "@/utils/request";
 
 export default class postAPI {
-  static async getPostList(page, limit, sort, order, search) {
-    return get("/post/getPostList", {
-      page: page,
-      limit: limit,
-      sort: sort,
-      order: order,
-      search: search,
-    });
-  }
   // 获取用户动态
   static async getPostByUserId(id) {
     return get("/post/getPostByUserId", {
@@ -20,21 +12,8 @@ export default class postAPI {
   static async getAllPost() {
     return get("/post/getAllPost");
   }
-  // 修改动态
-  static async updatePost(id, title, content, cover, tags, status) {
-    return get("/post/updatePost", {
-      id: id,
-      title: title,
-      content: content,
-      cover: cover,
-      tags: tags,
-      status: status,
-    });
-  }
-  // 删除动态
-  static async deletePost(id) {
-    return get("/post/deletePost", {
-      id: id,
-    });
+  // 发布动态
+  static async addPost(userPost) {
+    return post("/post/publishPost", userPost);
   }
 }
