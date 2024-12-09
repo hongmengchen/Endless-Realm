1d04cffd1deb41cacdd63fa6e971f0473acae119
hongmengchen <2560174991@qq.com>
2024-12-09 22:26:55 +0800

    feat(setting): 设置页面添加侧边栏组件
    - 在设置页面中添加了左侧侧边栏组件 AppSideBar
    - 优化了页面布局，分为侧边栏和主要内容区域
    - 移除了不必要的空模板和样式标签

--- frontend/src/views/setting.vue
+++ frontend/src/views/setting.vue
@@ -1,11 +1,22 @@
+<template>
+  <div class="container">
+    <!-- 左侧侧边栏 -->
+    <el-aside class="container-aside">
+      <AppSideBar />
+    </el-aside>
+
+    <!-- 右侧内容 -->
+    <el-main class="container-body">设置页面</el-main>
+  </div>
+</template>
+
 <script>
+import AppSideBar from "@/components/AppSideBar.vue";
+
 export default {
-  name: "userSetting",
+  name: "settingPage",
+  components: { AppSideBar },
 };
 </script>
 
-<template>
-  <div></div>
-</template>
-
-<style scoped></style>
+<style></style>
