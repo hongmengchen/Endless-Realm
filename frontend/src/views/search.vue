cddda9ee856b53f37f5020540915d31be6adebce
hongmengchen <2560174991@qq.com>
2024-12-09 22:27:32 +0800

    feat(search): 搭建搜索页面布局
    - 添加左侧侧边栏和右侧主要内容区域
    -引入 AppSideBar 组件- 优化模板结构，删除空模板

--- frontend/src/views/search.vue
+++ frontend/src/views/search.vue
@@ -1,11 +1,22 @@
+<template>
+  <div class="container">
+    <!-- 左侧侧边栏 -->
+    <el-aside class="container-aside">
+      <AppSideBar />
+    </el-aside>
+
+    <!-- 右侧内容 -->
+    <el-main class="container-body">搜索页面</el-main>
+  </div>
+</template>
+
 <script>
+import AppSideBar from "@/components/AppSideBar.vue";
+
 export default {
-  name: "userSearch",
+  name: "searchPage",
+  components: { AppSideBar },
 };
 </script>
-
-<template>
-  <div></div>
-</template>
 
 <style scoped></style>
