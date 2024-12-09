4fd49936324987a323f40004d6c259a07b9a0697
hongmengchen <2560174991@qq.com>
2024-12-09 23:07:23 +0800

    feat: 添加帖子详情页面
    - 新增 post-detail.vue 组件- 实现帖子内容、图片、点赞数和评论数的展示- 使用 PostAPI 获取帖子详情数据

---
+++ frontend/src/views/post-detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="post-detail">
+    <div v-if="post">
+      <p>内容：{{ post.content }}</p>
+      <img v-if="post.mediaUrl" :src="post.mediaUrl" alt="动态图片" />
+      <p>点赞数：{{ post.likeCount }}</p>
+      <p>评论数：{{ post.commentCount }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostAPI from "@/api/postAPI";
+
+export default {
+  name: "post-detail",
+  data() {
+    return {
+      post: null,
+    };
+  },
+  async created() {
+    const postId = this.$route.params.postId;
+    const res = await PostAPI.getPostDetail(postId);
+    this.post = res.data.data;
+  },
+};
+</script>
+
+<style scoped></style>
