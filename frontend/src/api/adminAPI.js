79d1b34919238c2df26f65dce2c3a941debe90fa
hongmengchen <2560174991@qq.com>
2024-12-12 16:43:04 +0800

    feat(api): 添加获取所有用户接口并优化代码结构
    - 新增 getAllUser 静态方法，用于根据账号状态获取所有用户- 将 login 方法改为静态方法，与 getAllUser 保持一致- 删除了 login 方法多余的注释，改为简洁的注释

--- frontend/src/api/adminAPI.js
+++ frontend/src/api/adminAPI.js
@@ -1,16 +1,17 @@
 import { get } from "@/utils/request";
 
 export default class AdminAPI {
-  /**
-   * 登录
-   * @param {String} username 用户名
-   * @param {String} password 密码
-   * @returns
-   */
+  // 管理员登录
   static async login(username, password) {
     return get("/admin/login", {
       username: username,
       password: password,
+    });
+  }
+  // 根据账号状态获取所有用户
+  static async getAllUser(status) {
+    return get("/admin/getAllUserByStatus", {
+      status: status,
     });
   }
 }
