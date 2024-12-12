ca5bdeb68f2e8a4f56fec2b2bde92d3841c9c338
hongmengchen <2560174991@qq.com>
2024-12-12 16:42:30 +0800

    feat(admin): 添加获取用户列表功能
    - 在 AdminController 中添加 getAllUserByStatus 方法
    - 通过 GET 请求获取指定状态的用户列表
    - 注入 UserService 并调用 getAllUserByStatus 方法

--- backend/src/main/java/cn/hmck/controller/AdminController.java
+++ backend/src/main/java/cn/hmck/controller/AdminController.java
@@ -1,7 +1,9 @@
 package cn.hmck.controller;
 
 import cn.hmck.entity.Admin;
+import cn.hmck.entity.User;
 import cn.hmck.service.AdminService;
+import cn.hmck.service.UserService;
 import cn.hmck.util.ErrorMsg;
 import cn.hmck.util.Result;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * 管理员控制器
@@ -26,9 +29,11 @@ public class AdminController {
     // 注入 AdminService
     @Resource(name = "adminService")
     private AdminService adminService;
+    // 注入 UserService
+    @Resource(name = "userService")
+    private UserService userService;
 
     // 管理员登录
-    @CrossOrigin(origins = "http://localhost:8081", methods = {RequestMethod.POST, RequestMethod.GET})
     @RequestMapping("/login")
     public Result<Admin> login(@RequestParam("username") @NotEmpty @NotNull String username,
                                @RequestParam("password") @NotEmpty @NotNull String password,
@@ -58,4 +63,12 @@ public class AdminController {
 
         return Result.success(admin);
     }
+
+    // 获取用户列表
+    @GetMapping("/getAllUserByStatus")
+    public Result<List<User>> getAllUserByStatus(@RequestParam("status") @NotEmpty @NotNull Integer status) {
+        System.out.println("获取用户列表：" + status);
+        return Result.success(userService.getAllUserByStatus(status));
+    }
+
 }
