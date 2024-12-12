e964adf84f0d8257c88b84157c75978b053645ec
hongmengchen <2560174991@qq.com>
2024-12-12 14:09:14 +0800

    feat(store): 添加管理员模块
    - 在 store 中引入并注册 adminRoute 模块
    - 用于管理后台相关数据和逻辑

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore, createLogger } from "vuex";
 import userStore from "./modules/userStore";
+import adminRoute from "./modules/adminStore";
 
 const debug = process.env.NODE_ENV !== "production";
 
 export default createStore({
   modules: {
     user: userStore, // 注册用户模块
+    admin: adminRoute, // 注册管理员模块
   },
   strict: debug,
   plugins: debug ? [createLogger()] : [],
