793249d38c75d4146d1a56013de92f7167aecba8
hongmengchen <2560174991@qq.com>
2024-12-12 14:08:10 +0800

    feat(router): 添加后台登录和管理页面的 meta 标题
    - 在 login-admin 路径中添加 meta 标题 "后台登录 | Endless Realm · 无垠之界"
    - 在 admin-platform路径中添加 meta 标题 "后台管理 | Endless Realm · 无垠之界"

--- frontend/src/router/modules/adminRoute.js
+++ frontend/src/router/modules/adminRoute.js
@@ -3,10 +3,16 @@ export default [
     path: "/login-admin",
     name: "login-admin",
     component: () => import("@/views/login-admin.vue"),
+    meta: {
+      title: "后台登录 | Endless Realm · 无垠之界",
+    },
   },
   {
     path: "/admin-platform",
     name: "admin-platform",
     component: () => import("@/views/admin-platform.vue"),
+    meta: {
+      title: "后台管理 | Endless Realm · 无垠之界",
+    },
   },
 ];
